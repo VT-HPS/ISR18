cc1ecbec76b5cbc7331933322b036187cf6d71ec
VTHPS-Electronics <vthpselectronics@gmail.com>
2024-10-18 14:52:54 -0400

    adding lights code to main

---
+++ Motors/lights.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO #imports servo library
+import time
+
+
+# code to program lights
+# figure out difference between duty cycle and 
+
+GPIO.setmode(GPIO.BCM) 
+GPIO_PIN = 24
+
+
+# Set the GPIO pin as an output
+GPIO.setup(GPIO_PIN, GPIO.OUT)
+
+# PWM?? - mimmum pwm signal
+pwm = GPIO.PWM(GPIO_PIN, 100)
+
+
+# Sleep time setting
+sleep_time = 1
+#pwm.start()
+#GPIO.output(GPIO_pin, GPIO.HIGH)
+#pwm.start(11)
+#13-17 is when the light is brightest, further testing for brightest signal
+pwm.start(11) # starts the signal at the minmum value (11-19)
+time.sleep(4)
+try:
+    while True: # dont need sleep time bc of duty cycle
+        pwm.ChangeDutyCycle(0)
+        pwm.ChangeDutyCycle(19)
+        pwm.ChangeDutyCycle(0)
+    
+
+finally:
+    # Clean up GPIO
+    pwm.stop()
+    GPIO.cleanup()
