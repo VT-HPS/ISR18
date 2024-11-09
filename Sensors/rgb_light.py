fdf49efe132d447142b72f511aae981a55f99d76
vthpselectronics@vt.edu <vthpselectronics@vt.edu>
2024-11-08 20:52:45 -0500

    need to run with sudo

--- Sensors/rgb_light.py
+++ Sensors/rgb_light.py
@@ -4,19 +4,19 @@ import RPi.GPIO
 import neopixel_write
 import board
 import digitalio
-from Battery_Warning import check_for_low_battery, check_for_dead_battery
+#from Battery_Warning import check_for_low_battery, check_for_dead_battery
 
 
 pin = digitalio.DigitalInOut(board.D18)#uses pin 18 as signal
 pin.direction = digitalio.Direction.OUTPUT
-#pixel_on = bytearray([0, 255, 0])
+pixel_on = bytearray([0, 255, 0])
 #pixel_off = bytearray([0,0,0])
 
 while(True):
-    if(check_for_low_battery == True):
-        pixel_on = bytearray([255, 255,0])#turns yellow if battery is low
-    elif(check_for_dead_battery == True):
-        pixel_on = bytearray([255,0,0])#turns red if battery is dead
-    else:
-        pixel_on = bytearray([0, 255, 0])#turns green if battery is ok
+    #if(check_for_low_battery == True):
+    #    pixel_on = bytearray([255, 255,0])#turns yellow if battery is low
+    #elif(check_for_dead_battery == True):
+        #pixel_on = bytearray([255,0,0])#turns red if battery is dead
+    #else:
+        #pixel_on = bytearray([0, 255, 0])#turns green if battery is ok
     neopixel_write.neopixel_write(pin, pixel_on)#lights up led
