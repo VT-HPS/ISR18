04f781be36628ec309041ef58cb7a7ea4bac09c2
VTHPS-Electronics <sanilindup@gmail.com>
2024-11-15 11:46:53 -0500

    minor tweaks to print statements

--- Sensors/python-BerryIMU-gyro-accel-compass/berryIMU.py
+++ Sensors/python-BerryIMU-gyro-accel-compass/berryIMU.py
@@ -281,27 +281,26 @@ while True:
 
 
 
-    if 1:                       #Change to '0' to stop showing the angles from the accelerometer
+    if 0:                       #Change to '0' to stop showing the angles from the accelerometer
         outputString += "#  ACCX Angle %5.2f ACCY Angle %5.2f  #  " % (AccXangle, AccYangle)
 
-    if 1:                       #Change to '0' to stop  showing the angles from the gyro
+    if 0:                       #Change to '0' to stop  showing the angles from the gyro
         outputString +="\t# GRYX Angle %5.2f  GYRY Angle %5.2f  GYRZ Angle %5.2f # " % (gyroXangle,gyroYangle,gyroZangle)
 
     if 1:                       #Change to '0' to stop  showing the angles from the complementary filter
         outputString +="\t#  CFangleX Angle %5.2f   CFangleY Angle %5.2f  #" % (CFangleX,CFangleY)
-        with open("cfangle.txt", "a") as f:
-            print(outputString, file=f)
+        ##with open("cfangle.txt", "a") as f:
+            ##print(outputString, file=f)
             ##print("I have a question.", file=f)
-        ##print(outputString)
 
 
     if 1:                       #Change to '0' to stop  showing the heading
         outputString +="\t# HEADING %5.2f  tiltCompensatedHeading %5.2f #" % (heading,tiltCompensatedHeading)
 
-    if 1:                       #Change to '0' to stop  showing the angles from the Kalman filter
+    if 0:                       #Change to '0' to stop  showing the angles from the Kalman filter
         outputString +="# kalmanX %5.2f   kalmanY %5.2f #" % (kalmanX,kalmanY)
 
-    
+    print(outputString)
 
     #slow program down a bit, makes the output more readable
     time.sleep(0.03)
