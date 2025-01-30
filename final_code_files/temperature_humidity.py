8234e561e446b11ae9edadf5bf6cf98bc5a0b254
calvintozer2024S <calvintozer@vt.edu>
2025-01-30 16:14:04 -0500

    Update temperature_humidity.py
    changed from print statements to return values

--- final_code_files/temperature_humidity.py
+++ final_code_files/temperature_humidity.py
@@ -4,12 +4,19 @@
 import board
 import busio
 import adafruit_sht31d
-import time
 
 def read_temperature():
     # Create sensor object, communicating over the board's default I2C bus
     i2c = board.I2C() # uses board.SCL and board.SDA
     # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
     sensor = adafruit_sht31d.SHT31D(i2c)
-    print("\nTemperature: %0.1f C" % sensor.temperature)
-    print("Humidity: %0.1f %%" % sensor.relative_humidity)
+    
+    return sensor.temperature
+
+def read_humidity():
+     # Create sensor object, communicating over the board's default I2C bus
+    i2c = board.I2C() # uses board.SCL and board.SDA
+    # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+    sensor = adafruit_sht31d.SHT31D(i2c)
+    
+    return sensor.relative_humidity
