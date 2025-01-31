c00aaba4fade708252ee3d2c14ac452261dcb423
hankhmitch <165087701+hankhmitch@users.noreply.github.com>
2025-01-31 12:23:44 -0500

    Update rgb_light.py for final code

--- Sensors/rgb_light.py
+++ Sensors/rgb_light.py
@@ -1,52 +1,38 @@
 from gpiozero import LED
-from time import sleep
 import RPi.GPIO
-#import neopixel_write
 import board
-import digitalio
 
-#from Battery_Warning import check_for_low_battery, check_for_dead_battery
-#from leak import leak as leak_check
+from sensor_manager import battery_voltage#check_for_low_battery, check_for_dead_battery
+from leak import leak as leak_check
 
-white_ledLeak = LED(24)
+white_ledLeak = LED(2)
 #yellow_ledLeak = LED(24) this is probably not needed since leak is either on or off
-red_ledLeak = LED((25)
+red_ledLeak = LED(3)
 
 green_ledBat = LED(17)
 yellow_ledBat = LED(27)
 red_ledBat = LED(22)
 
 
-while(True):#comment out this "while(True):" when combining code assuming this will just go on the main loop
+#while(True):#comment out this "while(True):" when combining code assuming this will just go on the main loop
 
-    #Actual lights code
-    #white_ledLeak.off()
-    #red_ledLeak.off()
-    
-    green_ledBat.off()
-    yellow_ledBat.off()
-    red_ledBat.off()
-    #battery lights
-    #if(check_for_low_battery == True):
-    #    yellow_ledBat.on()#turns yellow if battery is low
-    #elif(check_for_dead_battery == True):
-        #red_ledBat.on()#turns red if battery is dead
-    #else:
-        #green_ledBat.on()#turns green if battery is ok
+#switches lights if they should change
+white_ledLeak.off()
+red_ledLeak.off()
+green_ledBat.off()
+yellow_ledBat.off()
+red_ledBat.off()
 
-    #Leak lights
-    #if(leak_check == True):
-     #   red_ledLeak.on()
-    #else:
-     #   white_led.on()
-    
-    #Test code
-    white_ledLeak.on()
-    red_ledLeak.on()
-    
-    green_ledBat.on()
-    yellow_ledBat.on()
-    red_ledBat.on()
-    
-    print("working")
-    
+#battery lights
+if(battery_voltage < 11):
+red_ledBat.on()#turns red light on if battery is dead
+elif(battery_voltage < 11.5):
+red_ledBat.on()#turns ryellow on if battery is low
+else:
+green_ledBat.on()#turns green if battery is ok
+
+#Leak lights
+if(leak_check == True):
+red_ledLeak.on()#truns red light on if there is a leak
+else:
+white_ledLeak.on()#turns white light on if no leak
